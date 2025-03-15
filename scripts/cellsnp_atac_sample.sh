7440e0b40db70a6cb00b6f28b76924f783741c49
Yuxuan Song <yuxuans@gl-login3.arc-ts.umich.edu>
2025-03-14 22:41:55 -0400

    upload script

--- scripts/cellsnp_atac_sample.sh
+++ scripts/cellsnp_atac_sample.sh
@@ -5,13 +5,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=48:00:00
-#SBATCH --account=sigbio_project25
-#SBATCH --partition=sigbio
-#SBATCH --error=/home/yuxuans/log_file/cellsnp_error.log
+#SBATCH --account=slurm_account
+#SBATCH --partition=slurm_partition
+#SBATCH --error=/home/log_file/cellsnp_error.log
 #SBATCH --mem=80G
-#SBATCH --mail-user=yuxuans@umich.edu
 #SBATCH --export=ALL
-#SBATCH --output=/home/yuxuans/log_file/vcf_error.log
+#SBATCH --output=/home/log_file/vcf_error.log
 
 
 # cd $SLURM_SUBMIT_DIR
@@ -20,7 +19,7 @@ module load Bioinformatics
 module load samtools
 
 #index the bam file
-samtools index /scratch/sigbio_project_root/sigbio_project25/yuxuans/10x/bam/11008-LG-1_atac.bam
+samtools index /data/atac_sample.bam
 
-cellSNP -s /scratch/sigbio_project_root/sigbio_project25/yuxuans/10x/bam/11008-LG-1_atac.bam -b /scratch/sigbio_project_root/sigbio_project25/yuxuans/10x/barcode/filtered/11008-LG-1.tsv -O /scratch/sigbio_project_root/sigbio_project25/yuxuans/10x/vrieo_snp/out/11008/11008-LG-1/atac/pileup -R /scratch/sigbio_project_root/sigbio_project25/yuxuans/10x/vcf/output_filtered_new_11008.vcf -p 20 --minMAF 0.1 --minCOUNT 20 --UMItag None
+cellSNP -s /data/atac_sample.bam -b /barcode/barcodes.tsv.gz -O /out/atac/pileup -R sample.vcf -p 20 --minMAF 0.1 --minCOUNT 20 --UMItag None
 
